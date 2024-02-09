a75768fdb6eb73c5de905711271d22fa296b79ab
Yao Lu <95luyao@gmail.com>
2024-02-09 18:28:55 +0800

    7b benchmark

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -19,9 +19,9 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 32
+n_head = 32
+n_embd = 4096
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
